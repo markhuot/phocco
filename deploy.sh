6aa2deaf51c3f4eddf59c522c3fc65ad01de08d1
Mark Huot <mark@markhuot.com>
2012-01-21 16:24:22 +0000

    adding deployment helper

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp resources/phocco.css docs/phocco.css
+cp resources/showdown.js docs/showdown.js
+
+git checkout gh-pages
+
+cp docs/phocco.css resources/
+cp docs/showdown.js resources/
+
+cp docs/phocco.php.html .
+cp docs/phocco.php.html index.html
+cp docs/resources/showdown.js.html resources/
+cp docs/resources/phocco.css.html resources/
